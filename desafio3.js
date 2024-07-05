6bcd668102e2e56d60b623e9851dd244729b7a69
victordoui <victordoui@gmail.com>
2024-07-05 09:39:19 -0300

    Commit

---
+++ desafio3.js
@@ -0,0 +1,26 @@
+let posicaoNoMapa = 5
+
+let passosDados = 1
+
+const posicaoFinal = passosDados +
+    else;
+
+if (posicaoNoMapa === 1) {
+
+    console.log("Início da jornada")
+
+} else if (posicaoNoMapa + passosDados === 2) {
+
+    console.log("Obstáculo")
+
+} else if (posicaoNoMapa + passosDados === 3) {
+
+    console.log("Animais perigosos, fuja!")
+
+} else if (posicaoNoMapa + passosDados === 4) {
+
+    console.log("Obstáculo perigoso")
+
+} else {
+
+    console.log("Você foi encontrado no território do Dragão, prepare-se para batalha!")
